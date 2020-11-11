47f3c54092b200825e15a1ab0f06ba84df6fa2dd
Erwann <erwann.fady@gmail.com>
2020-11-11 01:17:27 +0100

    ajout waterfall

--- routes/playerControle.js
+++ routes/playerControle.js
@@ -2,6 +2,8 @@
 var bcrypt = require('bcrypt');
 var jwtUtils = require('../utils/jwt.utils');
 var models = require('../models');
+var asyncLib = require('async');
+
 
 //Routes
 module.exports = {
@@ -13,65 +15,88 @@ module.exports = {
             return res.status(400).json({'error': 'missing parameters'});
         }
 
-        models.Player.findOne({
-            attributes: ['pseudo'],
-            where: { pseudo: pseudo }
-        })
-            .then(function (userFound) {
-                if (!userFound) {
+        asyncLib.waterfall([
+            function (done) {
+                models.Player.findOne({
+                    attributes: ['pseudo'],
+                    where: {pseudo: pseudo}
+                });
+            }.then(function (playerFound) {
+                done(null, playerFound)
+            }).catch(function (err) {
+                return res.status(500).json({'error': err.message})
+            }),
+
+            function (playerFound, done) {
+                if (!playerFound) {
                     bcrypt.hash(password, 5, function (err, bcryptPassword) {
-                        console.log("ok");
-                        var newPlayer = models.Player.create({
-                            pseudo: pseudo,
-                            password: bcryptPassword,
-                            defeat: 0,
-                            victory: 0
-                        })
-                        .then(function (newUser) {
-                            return res.status(201).json({
-                                'playerId' : newUser.id
-                            })
-                       })
-                        .catch(function (err) {
-                            return res.status(500).json({
-                                'erreur' : 'cannot add user'
-                            })
-                        });
+                        done(null, playerFound, bcryptPassword)
+                    });
+                } else {
+                    return res.status(409).json({'error': 'player already existe'});
+                }
+            },
+
+            function (playerFound, bcryptPassword, done) {
+                var newPlayer = models.Player.create({
+                    pseudo: pseudo,
+                    password: bcryptPassword,
+                    defeat: 0,
+                    victory: 0
+                })
+                    .then(function (newPlayer) {
+                        done(newPlayer);
                     })
-                } else
-                    return res.status(409).json({ 'error' : 'pseudo already exist'});
-            })
-            .catch(function (err) {
-                return res.status(500).json({ 'error' : err.message});
-            });
+                    .catch(function (err) {
+                        return res.status(500).json({'error': err.message});
+                    })
+            },
+
+            function (newPlayer) {
+                if (newPlayer) {
+                    return res.status(201).json({
+                        'playerId': newPlayer.id
+                    });
+                } else {
+                    return res.status(500).json({'error': err.message});
+                }
+            }
+        ]);
     },
 
     login: function (req, res) {
         var pseudo = req.body.pseudo;
         var password = req.body.password;
 
-        models.Player.findOne({
-            where: { pseudo: pseudo }
-        }).then(function (userFound) {
-            if (userFound) {
+        asyncLib.waterfall([
+            function (done) {
+                models.Player.findOne({
+                    where: {pseudo: pseudo}
+                });
+            }.then(function (playerFound) {
+                done(null, playerFound);
+            }).catch(function (err) {
+                return res.status(404).json({'error': 'user doesn\'t existe'});
+            }),
 
-                bcrypt.compare(password, userFound.password, function (errBycrypt, resBycrypt) {
-                    if (resBycrypt) {
-                        return res.status(200).json({
-                            'playerId' : userFound.id,
-                            'token' : jwtUtils.generateTokenForUser(userFound)
-                        });
-                    } else {
-                        return res.status(403).json({ "error" : "invalid password" });
-                    }
+            function (playerFound, done) {
+                if (playerFound) {
+                    bcrypt.compare(password, playerFound.password, function (err, bcryptPassword) {
+                        done(null, playerFound, bcryptPassword);
+                    })
+                } else {
+                    return res.status(409).json({'error': 'player already existe'});
+                }
+            },
 
-                })
-            } else {
-                return res.status(404).json({ 'error' : 'user doesn\'t existe' });
+            function (playerFound, bcryptPassword, done) {
+                if (bcryptPassword) {
+                    return res.status(200).json({
+                        'playerId': playerFound.id,
+                        'token': jwtUtils.generateTokenForUser(playerFound)
+                    });
+                }
             }
-        })
-            .catch(function (err) {
-                return res.status(500).json({ 'error' : err.message});
-            })
+        ]);
     }
 }
